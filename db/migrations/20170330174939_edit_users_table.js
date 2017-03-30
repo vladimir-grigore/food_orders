5181f100623f303957623807dab1384d0f76dc35
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-30 18:26:55 +0000

    Alter the users migration

--- db/migrations/20170330174939_edit_users_table.js
+++ db/migrations/20170330174939_edit_users_table.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.alterTable("users", (table) => {
-    table.string('phone_number', 35).notNullable().alter();
+    table.string('phone_number', 35).alter();
   })
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema.alterTable("users", (table) => {
-    table.integer('phone_number');
+    table.integer('phone_number').alter();
   })
 };
