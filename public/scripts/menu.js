bf7a1f952bd01708acdad891224c27d5ba7b7a36
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-31 02:13:31 +0000

    Create insert request on orders table

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,26 +1,28 @@
 $(() => {
+
   $.ajax({
     method: "GET",
     url: "/menu"
   }).done((menu_items) => {
     for(item of menu_items) {
-      $("<div>").text(item.id).appendTo($(".menu-item"));
-      $("<div>").text(item.name).appendTo($(".menu-item"));
-      $("<div>").text(item.price).appendTo($(".menu-item"));
-      $("<div>").text(item.description).appendTo($(".menu-item"));
-      $("<div>").text(item.category_id).appendTo($(".menu-item"));
+      $("<div>").text(item.id).appendTo($("body"));
+      $("<div>").text(item.name).appendTo($("body"));
+      $("<div>").text(item.price).appendTo($("body"));
+      $("<div>").text(item.description).appendTo($("body"));
+      $("<div>").text(item.category_id).appendTo($("body"));
     }
   });
 
-  $('.login').on('submit', function(event){
+  $('#click-me').on('click', function(event){
     event.preventDefault();
 
     $.ajax({
       method: "POST",
-      url: "/login",
+      url: "/menu",
       data: $(this).serialize()
     }).done(function(){
-      window.location.href = '/'
+      console.log("DONE!!!");
     });
   });
+
 });
--- routes/menu_routes.js
+++ routes/menu_routes.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require('express');
+const moment  = require('moment-timezone');
 const router  = express.Router();
 
 module.exports = (knex) => {
@@ -18,17 +19,28 @@ module.exports = (knex) => {
     // TODO create entry in the orders table - get order_id
     // TODO create entry in the menu-items table
 
-  //   let reqUsername = req.body.username;
 
-  //   knex
-  //     .select("id")
-  //     .from("users")
-  //     .where('username', reqUsername)
-  //     .then((results) => {
-  //       // set the cookie for the user
-  //       res.cookie('user_id', results[0].id);
-  //       res.json(results);
-  //   });
+    let date = moment().tz("America/Vancouver").format();
+    console.log("DATE", date);
+    let user_id = req.cookies.user_id;
+    console.log("USER ID", user_id);
+    let payment_option = "in_person";
+
+    knex('orders')
+      .insert({user_id: user_id, payment_option: payment_option, placed_at: date})
+      .then((results) => {
+        console.log(results);
+      })
+
+    // knex
+    //   .select("id")
+    //   .from("users")
+    //   .where('username', reqUsername)
+    //   .then((results) => {
+    //     // set the cookie for the user
+    //     res.cookie('user_id', results[0].id);
+    //     res.json(results);
+    // });
   });
   return router;
 }
