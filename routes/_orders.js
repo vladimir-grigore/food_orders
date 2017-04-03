bfc0fc4cd3ee11e385311be6243b72fa1821de7e
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-04-03 08:13:33 +0000

    Twilio helper ready

--- routes/_orders.js
+++ routes/_orders.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router  = express.Router();
 const moment  = require('moment-timezone');
+const twilio_helper = require('./twilio_helper');
 
 module.exports = (knex) => {
 
@@ -55,6 +56,7 @@ module.exports = (knex) => {
       }).catch((err) => {
         return console.error(err);
       });
+      twilio_helper.text(time);
   });
 
   router.post("/complete/:id", (req, res) => {
@@ -69,6 +71,7 @@ module.exports = (knex) => {
       }).catch((err) => {
         return console.error(err);
       });
+      twilio_helper.orderReady("Your order is ready. Come and get it!");
   });
 
   return router;
